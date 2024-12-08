357471e7c68236b714483d8585afc5223620b716
思远久安 <2900412443@qq.com>
2024-12-08 18:34:11 +0800

    part1

--- account_web/req/loginParams.go
+++
@@ -1,6 +0,0 @@
-package req
-
-type LoginByPassword struct {
-	Mobile string `json:"mobile" binding:"required"`
-	Passwd string `json:"passwd" binding:"required, min=6, max=16"`
-}
--- account_web/res/account.go
+++
@@ -1,7 +0,0 @@
-package res
-
-type Account4Res struct {
-	Mobile   string `json:"mobile"`
-	NickName string `json:"nick_name"`
-	Gender   string `json:"gender"`
-}
